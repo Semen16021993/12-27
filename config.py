cacd0f465c3e26e053ba94d326470b2072968c23
Семен <semen@MacBook-Air.local>
2026-03-13 11:05:00 +0300

    add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# TELEGRAM BOT TOKEN
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+# OPENAI API KEY
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# ОСНОВНЫЕ ПАПКИ ПРОЕКТА
+CASES_FOLDER = "cases"
+RAG_FOLDER = "rag"
